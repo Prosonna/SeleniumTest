4f80690747cbf1af80743c7c17e278d57782b481
Prosonna Hossain Nabila <prosonna.nabila@gmail.com>
2018-02-19 16:54:08 +0600

    Adding all the files

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        //Create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+         
+
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened URL");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform Ops
+            element.SendKeys("Execute Automation");
+
+            Console.WriteLine("Executed Test");
+        
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Executed Test2");
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+            Console.WriteLine("Close the browser");
+        }
+    }
+}
